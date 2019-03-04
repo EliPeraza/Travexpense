ac1412ce6d93ac828ec6ffea90f9a9dc08996915
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-03-03 23:08:53 -0500

    expandable table with mock data working

--- Travexpense/Controllers/TravelerBalanceController.swift
+++ Travexpense/Controllers/TravelerBalanceController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+struct CellExpansion {
+  var opened: Bool
+  var title: String
+  var sectionData = [String]()
+}
+
 
 class TravelerBalanceController: UIViewController {
 
   var travelerSentFromOverview: TravelerInfo!
 
+  var tableViewData = [CellExpansion]()
   
   @IBOutlet weak var currentUserImage: UIImageView!
   @IBOutlet weak var currentUserName: UILabel!
@@ -31,23 +38,61 @@ class TravelerBalanceController: UIViewController {
       tableView.dataSource = self
       tableView.delegate = self
       
-    self.currentUserName.text = travelerSentFromOverview.title
+      tableViewData = [CellExpansion(opened: false, title: "title 1", sectionData: ["Cell 1", "Cell 2", "Cell 3"]),
+                       CellExpansion(opened: false, title: "title 2", sectionData: ["Cell 1", "Cell 2", "Cell 3"]),
+                       CellExpansion(opened: false, title: "title 3", sectionData: ["Cell 1", "Cell 2", "Cell 3"]),
+                       CellExpansion(opened: false, title: "title 4", sectionData: ["Cell 1", "Cell 2", "Cell 3"])]
+      
+    setupUI()
       
     }
+  
+  func setupUI() {
+    self.currentUserName.text = "Eli"
+    self.userWhoseBalanceLabel.text = travelerSentFromOverview.title
+  }
   
 }
 
 extension TravelerBalanceController: UITableViewDataSource, UITableViewDelegate {
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return tableViewData.count
+  }
+  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
+    if tableViewData[section].opened {
+      return tableViewData[section].sectionData.count + 1
+    } else {
+     return 1
+    }
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: "BalanceCell") else {return UITableViewCell()}
     
-   cell.textLabel?.text = "hello"
+    if indexPath.row == 0 {
+      cell.textLabel?.text = tableViewData[indexPath.section].title
+      return cell
+    } else {
+      cell.textLabel?.text = tableViewData[indexPath.section].sectionData[indexPath.row - 1]
+      return cell
+    }
     
-    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.row ==  0{
+      if tableViewData[indexPath.section].opened == true {
+        tableViewData[indexPath.section].opened = false
+        let sections = IndexSet.init(integer: indexPath.section)
+        tableView.reloadSections(sections, with: .none)
+      } else {
+        tableViewData[indexPath.section].opened = true
+        let sections = IndexSet.init(integer: indexPath.section)
+        tableView.reloadSections(sections, with: .none)
+      }
+    }
   }
 
 
