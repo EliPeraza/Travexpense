f952272d2117b6353cdfa8aa205bdd6de241eb95
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-03-06 16:17:26 -0500

    configured buttons and amount textfield

--- Travexpense/Controllers/EnterExpenseController.swift
+++ Travexpense/Controllers/EnterExpenseController.swift
@@ -12,11 +12,14 @@ class EnterExpenseController: UIViewController {
   
   //Var to save object to Firebase
   var expenseCategory = String()
+  var expenseDescription =  String()
+  var expenseAmount = String()
+  var travelersToShareExpense = [String]()
   
   
   private var amountTextFieldPlaceholder = "Enter total amount for the expense"
   private var expenseDescriptionPlaceHolder = "Enter expense description. Example: 'Train to Paris'"
-
+  
   var travelerModel = TravelerModel()
   
   @IBOutlet var arrayOfExpenseCategoryButton: [UIButton]!
@@ -33,7 +36,7 @@ class EnterExpenseController: UIViewController {
   @IBOutlet weak var saveExpenseButton: UIButton!
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
     
     expenseCategoryButton.setTitle("Select Expense Category", for: .normal)
     
@@ -45,9 +48,16 @@ class EnterExpenseController: UIViewController {
     
     
     configureTextFields()
+    configureExpenseCategoryButton()
     
-   
+    amountTextField.addTarget(self, action: #selector(myTextFieldDidChange(_:)), for: .editingChanged)
+  }
+  
+  @objc func myTextFieldDidChange(_ textField: UITextField) {
+    if let amountString = textField.text?.currencyInputFormatting() {
+      textField.text = amountString
     }
+  }
   
   func configureTextFields() {
     amountTextField.text = amountTextFieldPlaceholder
@@ -59,42 +69,70 @@ class EnterExpenseController: UIViewController {
     expenseDescriptionField.textColor = .lightGray
     
   }
-
+  
   
   @IBAction func expenseCategoryButtonPressed(_ sender: UIButton) {
-    arrayOfExpenseCategoryButton.forEach { (button) in
-      UIView.animate(withDuration: 0.3, animations: {
-        button.isHidden = !button.isHidden
-        self.view.layoutIfNeeded()
-      })
-    }
+    expenseCategoryButtonAnimation()
+  }
+  
+  
+//  @IBAction func selectionOfExpenseCategory(_ sender: UIButton) {
+//    let index = sender.tag
+//    expenseCategory = arrayOfExpenseCategoryButton[index].titleLabel?.text ?? "Tapped didn't work"
+//  }
+  
+//
+//  @IBAction func selectTravelersButtonPressed(_ sender: UIButton) {
+//    print(travelerModel.selectedItems.map{$0.title})
+//
+//  }
+  
+  @IBAction func saveSelectedButton(_ sender: UIButton) {
+    let buttonTitle = sender.titleLabel?.text ?? "Button doesn't have a title"
     
+    switch buttonTitle.lowercased(){
+    case "transportation":
+     updateButtonConfiguration(buttonTitle: buttonTitle)
+    case "lodging":
+      updateButtonConfiguration(buttonTitle: buttonTitle)
+    case "entertainment":
+      updateButtonConfiguration(buttonTitle: buttonTitle)
+    case "food":
+      updateButtonConfiguration(buttonTitle: buttonTitle)
+    default:
+      print("THE SWITCH IS NOT WORKING")
+    }
   }
   
   
   
-  @IBAction func selectionOfExpenseCategory(_ sender: UIButton) {
-    let index = sender.tag
-    expenseCategory = arrayOfExpenseCategoryButton[index].titleLabel?.text ?? "Tapped didn't work"
-    dump (expenseCategory)
-    }
-  
-  
-  @IBAction func selectTravelersButtonPressed(_ sender: UIButton) {
-    print(travelerModel.selectedItems.map{$0.title})
-    
-  }
-  
-  
-  
-  func configureExpenseCategoryButton() {
+  private func configureExpenseCategoryButton() {
     var index = 0
     for expenseCategory in ExpenseType.allCases {
       arrayOfExpenseCategoryButton[index].setTitle(expenseCategory.rawValue, for: .normal)
       index += 1
     }
   }
-
+  
+  private func updateButtonConfiguration(buttonTitle: String) {
+    expenseCategory = buttonTitle
+    expenseCategoryButton.setTitle(buttonTitle, for: .normal)
+    expenseCategoryButtonAnimation()
+    arrayOfExpenseCategoryButton.forEach { (button) in
+      button.isEnabled = false
+    }
+  }
+  
+  
+  private func expenseCategoryButtonAnimation(){
+    arrayOfExpenseCategoryButton.forEach { (button) in
+      UIView.animate(withDuration: 0.3, animations: {
+        button.isHidden = !button.isHidden
+        self.view.layoutIfNeeded()
+      })
+    }
+  }
+  
 }
 
 extension EnterExpenseController: UITableViewDelegate {
@@ -102,14 +140,14 @@ extension EnterExpenseController: UITableViewDelegate {
     
     travelerModel.travelerInfo[indexPath.row].isSelected = true
     
-     saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
+    saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
     
     
   }
   func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
     travelerModel.travelerInfo[indexPath.row].isSelected = false
-
-     saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
+    
+    saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
   }
   
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -146,15 +184,14 @@ extension EnterExpenseController: UITextFieldDelegate {
     }
     
     if textField.text == amountTextFieldPlaceholder {
-     textField.textColor = .black
+      textField.textColor = .black
       textField.text = ""
     }
     
-  
+    
     
   }
   func textFieldDidEndEditing(_ textField: UITextField) {
-    
     if textField.text == "" {
       textField.textColor = .lightGray
       textField.text = expenseDescriptionPlaceHolder
@@ -162,6 +199,36 @@ extension EnterExpenseController: UITextFieldDelegate {
     }
     
     
-    
   }
 }
+
+extension String {
+  
+  /*Source: https://github.com/vivatum/Currency_Format_from_left_to_right/blob/master/TextFieldCurrencyFormat/ViewController.swift*/
+    
+  func currencyInputFormatting() -> String {
+    
+    var number: NSNumber!
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currencyAccounting
+    formatter.currencySymbol = "$"
+    formatter.maximumFractionDigits = 2
+    formatter.minimumFractionDigits = 2
+    
+    var amountWithPrefix = self
+    
+    // remove from String: "$", ".", ","
+    let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+    amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
+    
+    let double = (amountWithPrefix as NSString).doubleValue
+    number = NSNumber(value: (double / 100))
+    
+    // if first number is 0 or all numbers were deleted
+    guard number != 0 as NSNumber else {
+      return ""
+    }
+    
+    return formatter.string(from: number)!
+  }
+  }
