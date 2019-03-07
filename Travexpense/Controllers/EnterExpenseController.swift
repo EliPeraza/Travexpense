4df0af40f3d22d318e6e87737a38862f176c74b2
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-03-06 20:11:52 -0500

    handled selectgn travelers from table, and text fields

--- Travexpense/Controllers/EnterExpenseController.swift
+++ Travexpense/Controllers/EnterExpenseController.swift
@@ -13,8 +13,9 @@ class EnterExpenseController: UIViewController {
   //Var to save object to Firebase
   var expenseCategory = String()
   var expenseDescription =  String()
-  var expenseAmount = String()
+  var expenseAmount = Double()
   var travelersToShareExpense = [String]()
+  var dictionaryOfTravelers = [String:String]()
   
   
   private var amountTextFieldPlaceholder = "Enter total amount for the expense"
@@ -33,7 +34,11 @@ class EnterExpenseController: UIViewController {
   @IBOutlet weak var tableToSelectTravelers: UITableView!
   
   
-  @IBOutlet weak var saveExpenseButton: UIButton!
+  @IBOutlet weak var saveSelectedTravelersButton: UIButton!
+  
+  @IBOutlet weak var saveExpenseEntry: UIButton!
+  
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -44,30 +49,24 @@ class EnterExpenseController: UIViewController {
     tableToSelectTravelers.delegate = self
     tableToSelectTravelers.dataSource = self
     
-    saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
+    saveSelectedTravelersButton.isEnabled = !travelerModel.selectedItems.isEmpty
     
     
     configureTextFields()
     configureExpenseCategoryButton()
     
     amountTextField.addTarget(self, action: #selector(myTextFieldDidChange(_:)), for: .editingChanged)
+    
   }
   
   @objc func myTextFieldDidChange(_ textField: UITextField) {
-    if let amountString = textField.text?.currencyInputFormatting() {
-      textField.text = amountString
+    expenseAmount = Double(textField.text ?? "nothing") ?? 0.0
+    
+    if let amountString = textField.text{
+      
+      textField.text = amountString.currencyInputFormatting()
+      print("This is the xpense amount to be saved \(expenseAmount)")
     }
-  }
-  
-  func configureTextFields() {
-    amountTextField.text = amountTextFieldPlaceholder
-    amountTextField.delegate = self
-    amountTextField.textColor = .lightGray
-    
-    expenseDescriptionField.text = expenseDescriptionPlaceHolder
-    expenseDescriptionField.delegate = self
-    expenseDescriptionField.textColor = .lightGray
-    
   }
   
   
@@ -76,23 +75,12 @@ class EnterExpenseController: UIViewController {
   }
   
   
-//  @IBAction func selectionOfExpenseCategory(_ sender: UIButton) {
-//    let index = sender.tag
-//    expenseCategory = arrayOfExpenseCategoryButton[index].titleLabel?.text ?? "Tapped didn't work"
-//  }
-  
-//
-//  @IBAction func selectTravelersButtonPressed(_ sender: UIButton) {
-//    print(travelerModel.selectedItems.map{$0.title})
-//
-//  }
-  
   @IBAction func saveSelectedButton(_ sender: UIButton) {
     let buttonTitle = sender.titleLabel?.text ?? "Button doesn't have a title"
     
     switch buttonTitle.lowercased(){
     case "transportation":
-     updateButtonConfiguration(buttonTitle: buttonTitle)
+      updateButtonConfiguration(buttonTitle: buttonTitle)
     case "lodging":
       updateButtonConfiguration(buttonTitle: buttonTitle)
     case "entertainment":
@@ -104,7 +92,68 @@ class EnterExpenseController: UIViewController {
     }
   }
   
+  @IBAction func selectTravelersButtonPressed(_ sender: UIButton) {
+    
+    travelersToShareExpense = travelerModel.selectedItems.map{$0.title}
+    
+    let displayNames = travelersToShareExpense.joined(separator: ", ")
+    
+    showAlert(title: "Split this expense among:", message: displayNames) { (alert) in
+      let ok = UIAlertAction(title: "Ok", style: .default) { (done) in
+        
+        
+        for person in self.travelersToShareExpense {
+          self.dictionaryOfTravelers[person] = person
+          print(self.dictionaryOfTravelers)
+        }
+
+        }
+
+      let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+      alert.addAction(ok)
+      alert.addAction(cancel)
+      self.present(alert, animated: true, completion: nil)
+    }
+    
+    print("travelers sharing expense: \(travelersToShareExpense)")
+    
+    
+  }
   
+  
+  @IBAction func saveExpenseCompleteEntryButtonPressed(_ sender: UIButton) {
+    
+    
+    guard let expenseDescription = expenseDescriptionField.text,
+      !expenseDescription.isEmpty else {
+        showAlert(title: "Missing Description", message: "Please enter more info")
+        return
+    }
+    
+    guard !travelersToShareExpense.isEmpty else {
+     showAlert(title: "Select Travelers", message: "Select and add travelers you are sharing the expense with")
+      return
+    }
+    
+    
+    let expense = ExpenseModel.init(expenseCategory: expenseCategory, expenseSubcategory: "nothing-refactor", expenseDescription: expenseDescription, expenseAmount: expenseAmount, travelersSharingExpense: ["Ess" : "Oli"])
+    
+    print("This is an expense\(expense)")
+    
+  }
+  
+  
+  
+  
+  private func configureTextFields() {
+    amountTextField.text = amountTextFieldPlaceholder
+    amountTextField.delegate = self
+    amountTextField.textColor = .lightGray
+    
+    expenseDescriptionField.text = expenseDescriptionPlaceHolder
+    expenseDescriptionField.delegate = self
+    expenseDescriptionField.textColor = .lightGray
+  }
   
   private func configureExpenseCategoryButton() {
     var index = 0
@@ -132,7 +181,6 @@ class EnterExpenseController: UIViewController {
       })
     }
   }
-  
 }
 
 extension EnterExpenseController: UITableViewDelegate {
@@ -140,14 +188,14 @@ extension EnterExpenseController: UITableViewDelegate {
     
     travelerModel.travelerInfo[indexPath.row].isSelected = true
     
-    saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
-    
+    saveSelectedTravelersButton.isEnabled = !travelerModel.selectedItems.isEmpty
     
   }
+  
   func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
     travelerModel.travelerInfo[indexPath.row].isSelected = false
     
-    saveExpenseButton.isEnabled = !travelerModel.selectedItems.isEmpty
+    saveSelectedTravelersButton.isEnabled = !travelerModel.selectedItems.isEmpty
   }
   
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -177,6 +225,18 @@ extension EnterExpenseController: UITableViewDataSource {
 }
 
 extension EnterExpenseController: UITextFieldDelegate {
+  
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    let characterLimit = 150
+    let startingLength = expenseDescriptionField.text?.count ?? 0
+    let lengthToAdd = string.count
+    let lengthToReplace = range.length
+    let newLength = startingLength + lengthToAdd - lengthToReplace
+    return newLength <= characterLimit
+    
+  }
+  
   func textFieldDidBeginEditing(_ textField: UITextField) {
     if textField.text == expenseDescriptionPlaceHolder {
       textField.textColor = .black
@@ -184,10 +244,10 @@ extension EnterExpenseController: UITextFieldDelegate {
     }
     
     if textField.text == amountTextFieldPlaceholder {
+      becomeFirstResponder()
       textField.textColor = .black
       textField.text = ""
     }
-    
     
     
   }
@@ -197,20 +257,29 @@ extension EnterExpenseController: UITextFieldDelegate {
       textField.text = expenseDescriptionPlaceHolder
       textField.text = amountTextFieldPlaceholder
     }
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField == amountTextField {
+      expenseDescriptionField.becomeFirstResponder()
+    }
     
-    
+    if textField == expenseDescriptionField {
+      self.view.endEditing(true)
+    }
+    return true
   }
 }
 
 extension String {
   
   /*Source: https://github.com/vivatum/Currency_Format_from_left_to_right/blob/master/TextFieldCurrencyFormat/ViewController.swift*/
-    
+  
   func currencyInputFormatting() -> String {
     
     var number: NSNumber!
     let formatter = NumberFormatter()
-    formatter.numberStyle = .currencyAccounting
+    formatter.numberStyle = .currency
     formatter.currencySymbol = "$"
     formatter.maximumFractionDigits = 2
     formatter.minimumFractionDigits = 2
@@ -231,4 +300,49 @@ extension String {
     
     return formatter.string(from: number)!
   }
-  }
+}
+
+
+//  @IBAction func amountTextAction(_ sender: UITextField) {
+//    let text = sender.text ?? "nothing"
+//    cleanDollars(text)
+//    expenseAmount = Double(cleanDollars(text)) ?? 0.0
+//  }
+//
+//  func cleanDollars(_ value: String?) -> String {
+//    guard value != nil else { return "$0.00" }
+//    let doubleValue = Double(value!) ?? 0.0
+//    let formatter = NumberFormatter()
+//    formatter.currencyCode = "USD"
+//    formatter.currencySymbol = "$"
+//    formatter.minimumFractionDigits = (value!.contains(".00")) ? 0 : 2
+//    formatter.maximumFractionDigits = 2
+//    formatter.numberStyle = .currencyAccounting
+//    return formatter.string(from: NSNumber(value: doubleValue)) ?? "$\(doubleValue)"
+//  }
+
+
+
+//    @IBAction func selectionOfExpenseCategory(_ sender: UIButton) {
+//      let index = sender.tag
+//      expenseCategory = arrayOfExpenseCategoryButton[index].titleLabel?.text ?? "Tapped didn't work"
+//    }
+//
+//
+
+
+
+/*  var expenseCategory = String()
+ var expenseDescription =  String()
+ var expenseAmount = String()
+ var travelersToShareExpense = [String]()*/
+
+
+//  @objc func expenseDescriptionEditingEnded(_ textField: UITextField){
+//    if let expenseDescriptionUnwrapped = expenseDescriptionField.text {
+//     expenseDescription = expenseDescriptionUnwrapped
+//      print("this is the expense description to save \(expenseDescription)")
+//      resignFirstResponder()
+//    }
+//  }
+//    expenseDescriptionField.addTarget(self, action: #selector(expenseDescriptionEditingEnded(_:)), for: .editingDidEnd)
